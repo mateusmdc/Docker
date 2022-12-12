3783ca47afd8d56cef31b51256671d33278d90ba
Mateus M. da Costa <mateus.costa@etice.local>
2022-12-12 11:59:47 -0300

    added folder and file for class 04 from php course

---
+++ .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 4 - Variáveis em PHP/tipos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+        $nome = "Mateus";
+        $idade = 24;
+        $nome1 = "Cris";
+        $idade1 = 21;
+        echo $nome . " tem " . $idade . " anos!";
+        echo "$nome1 tem $idade1 anos!";
+        echo '$nome tem $idade anos';
+        ?>
+    </div>
+</body>
+</html>
