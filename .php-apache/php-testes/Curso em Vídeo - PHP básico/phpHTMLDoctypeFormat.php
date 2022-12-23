29ac9260dfa76a9a9a2f7fe9dcb3715cf419d50b
Mateus M. da Costa <mateus.costa@etice.local>
2022-12-23 11:41:25 -0300

    created file for formatting php-html documents

---
+++ .php-apache/php-testes/Curso em Vídeo - PHP básico/phpHTMLDoctypeFormat.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+        
+        ?>
+    </div>
+</body>
+</html>
