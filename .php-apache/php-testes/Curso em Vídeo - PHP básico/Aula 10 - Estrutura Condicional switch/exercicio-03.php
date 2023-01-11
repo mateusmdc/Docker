001eea49900118398cdff9238b1bcb3ef7fce7bf
mateusmdc <mateus_serios@hotmail.com>
2023-01-11 11:36:25 -0300

    Added code for exercise 03 from class 10 from php course

--- .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 10 - Estrutura Condicional switch/exercicio-03.php
+++ .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 10 - Estrutura Condicional switch/exercicio-03.php
@@ -7,8 +7,49 @@
 <body>
     <div>
         <?php
-        
+            $e = isset($_GET["estado"]) ? $_GET["estado"] : "Não informado";
+            switch($e){
+                case "AC":
+                case "AM":
+                case "AP":
+                case "PA":
+                case "RO":
+                case "RR":
+                case "TO":
+                    $regiao = "Região Norte";
+                    break;
+                case "AL":
+                case "BA":
+                case "CE":
+                case "MA":
+                case "PB":
+                case "PE":
+                case "RN":
+                case "PI":
+                case "SE":
+                    $regiao = "Região Nordeste";
+                    break;
+                case "DF":
+                case "GO":
+                case "MT":
+                case "MS":
+                    $regiao = "Região Centro-Oeste";
+                    break;
+                case "MG":
+                case "ES":
+                case "RJ":
+                case "SP":
+                    $regiao = "Região Sudeste";
+                    break;
+                case "PR":
+                case "RS":
+                case "SC":
+                    $regiao = "Região Sul";
+                    break;
+            }
+            echo "Você mora na $regiao<br/>";
         ?>
+        <a href="exercicio-03.html">Voltar</a>
     </div>
 </body>
 </html>
