128651e410ceee33ad803833bcaacc5180571dd2
mateusmdc <mateus_serios@hotmail.com>
2023-01-11 11:49:55 -0300

    exercise 01 from class 11 from php course

---
+++ .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 11 - Estrutura de Repetição While/01-contador.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+            $c = 1;
+            while($c <= 10){
+                echo $c . "<br/>";
+                $c++;
+            }
+            $d = 10;
+            while($d >= 1){
+                echo $d . "<br/>";
+                $d--;
+            }
+        ?>
+    </div>
+</body>
+</html>
