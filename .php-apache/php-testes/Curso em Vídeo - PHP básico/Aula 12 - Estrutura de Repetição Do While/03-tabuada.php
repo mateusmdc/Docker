acdcf014c41cb390c8463362a75ec9592613a014
mateusmdc <mateus_serios@hotmail.com>
2023-01-13 09:57:12 -0300

    final changes made in files for class 12 from php course

--- .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 12 - Estrutura de Repetição Do While/03-tabuada.php
+++ .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 12 - Estrutura de Repetição Do While/03-tabuada.php
@@ -8,8 +8,22 @@
     <div>
         <?php
             $num = isset($_GET["numero"]) ? $_GET["numero"] : 0;
-            
+            $tab = 1;
+            echo "<h1>Mostrando a Tabuada de $num</h1><br/>";
+            if($num == 0){
+                do{
+                    echo "$num x $tab = " . ($num * $tab) . "<br/>";
+                    $tab++;
+                    }while($tab <= 10);
+            }
+            else{
+                do{
+                    echo "$num x $tab = " . ($num * $tab) . "<br/>";
+                    $tab++;
+                    }while($tab <= 10);
+            }
         ?>
+        <a href="03-index.php">Voltar</a>
     </div>
 </body>
 </html>
