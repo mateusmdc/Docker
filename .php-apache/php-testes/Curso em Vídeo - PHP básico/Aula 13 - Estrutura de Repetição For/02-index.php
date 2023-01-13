2dc9546784630ccf778b66988a4c76fa0e562b38
mateusmdc <mateus_serios@hotmail.com>
2023-01-13 11:11:18 -0300

    folder and files for class 13 from php course

---
+++ .php-apache/php-testes/Curso em Vídeo - PHP básico/Aula 13 - Estrutura de Repetição For/02-index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <meta charset="UTF-8"/>
+    <title></title>
+</head>
+<body>
+    <div>
+        <form method="get" action="02-tabuada.php">
+            <select name="num">
+            <?php
+                for($c = 1; $c <= 10; $c++){
+                    echo "<option>$c</option>";
+                }
+            ?>
+            </select>
+            <input type="submit" value="Tabuada"/>
+        </form>
+    </div>
+</body>
+</html>
